bcc80df7e28b3d41041089ad83c34e043200c63a
Michael Masson <10071388+michaelmass@users.noreply.github.com>
2022-07-28 15:03:58 -0400

    feat(trail): add trail package (#5)

---
+++ trail/readme.md
@@ -0,0 +1,36 @@
+# Trail
+> Opentelemetry initialization package for nodejs
+
+## Usage
+
+Trail is a utility to initialize the Opentelemetry library. First, you must install the instrumentation packages of the library you want to instrument.
+
+For example, you could install the package `@opentelemetry/instrumentation-express` to instrument `express`. Then, your TypeScript program could look like this:
+
+```ts
+import { init } from '@bpinternal/trail';
+init();
+
+import express from 'express';
+```
+
+The available environment variables for configuring the tracing client are:
+
+- TRACING_ENABLED `bool` Enables the tracer
+- TRACING_DEBUG `bool` Adds debug information about the tracing configuration
+- OTEL_EXPORTER_JAEGER_ENDPOINT `url` Sets the Jaeger collector endpoint
+- OTEL_SERVICE_NAME `string` Sets the service name given to a trace
+- OTEL_SERVICE_VERSION `string` Sets the current running version of the service
+- OTEL_SERVICE_VERSION_INSTANCE_ID `string` Sets the node intance id on which the service is running on
+- OTEL_SERVICE_NAMESPACE `string` Sets the namespace of the service
+- OTEL_DEPLOYMENT_ENVIRONMENT `string` Sets the environment of the service
+
+## Disclaimer ⚠️
+
+This package is published under the `@bpinternal` organization. All packages of this organization are meant to be used by the [Botpress](https://github.com/botpress/botpress) team internally and are not meant for our community. However, these packages were still left intentionally public for an important reason : We Love Open-Source. Therefore, if you wish to install this package feel absolutly free to do it. We strongly recomand that you tag your versions properly.
+
+The Botpress Engineering team.
+
+## Licensing
+
+This software is protected by the same license as the [main Botpress repository](https://github.com/botpress/botpress). You can find the license file [here](https://github.com/botpress/botpress/blob/master/LICENSE).
---
+++ trail/src/trace.ts
@@ -0,0 +1,83 @@
+import { registerInstrumentations } from "@opentelemetry/instrumentation";
+import { BatchSpanProcessor } from "@opentelemetry/sdk-trace-base";
+import { NodeTracerProvider } from "@opentelemetry/sdk-trace-node";
+import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
+import { JaegerPropagator } from "@opentelemetry/propagator-jaeger";
+import { defaultServiceName, Resource } from "@opentelemetry/resources";
+import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
+import { diag, DiagConsoleLogger, DiagLogLevel } from "@opentelemetry/api";
+import { JaegerExporter } from "@opentelemetry/exporter-jaeger";
+import yn from "yn";
+
+const isEnabled = (enabled?: boolean) => enabled !== undefined ? enabled : yn(process.env.TRACING_ENABLED, { default: false });
+const isDebugEnabled = (enabled?: boolean) => enabled !== undefined ? enabled : yn(process.env.TRACING_DEBUG, { default: false });
+
+const removeUndefinedValues = <T>(obj: Record<string, T | undefined>): Record<string, T> => {
+  const result: Record<string, T> = {};
+  Object.entries(obj).forEach(([key, value]) => {
+    if (value !== undefined) {
+      result[key] = value;
+    }
+  })
+  return result;
+}
+
+let initialized = false;
+
+type InitProps = {
+  debug?: boolean;
+  enabled?: boolean;
+  environment?: string;
+  serviceName?: string;
+  serviceNamespace?: string;
+  serviceVersion?: string;
+  serviceInstanceId?: string;
+}
+
+/**
+ * Warning: This function must be called synchronously at the start of the application.
+ * Since it creates hooks in other packages, it must be executed before any other imports.
+ */
+export const init = ({ 
+  debug, 
+  enabled,
+  environment,
+  serviceName,
+  serviceNamespace,
+  serviceVersion,
+  serviceInstanceId,
+}: InitProps = {}) => {
+  if (!isEnabled(enabled) || initialized) {
+    return;
+  }
+
+  if (isDebugEnabled(debug)) {
+    diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
+  }
+
+  const propagator = new JaegerPropagator();
+  const exporter = new JaegerExporter();
+  const processor = new BatchSpanProcessor(exporter);
+
+  const resourceAttributes = removeUndefinedValues({
+    [SemanticResourceAttributes.SERVICE_NAME]: serviceName ?? process.env.OTEL_SERVICE_NAME ?? defaultServiceName(),
+    [SemanticResourceAttributes.SERVICE_VERSION]: serviceVersion ?? process.env.OTEL_SERVICE_VERSION,
+    [SemanticResourceAttributes.SERVICE_INSTANCE_ID]: serviceInstanceId ?? process.env.OTEL_SERVICE_VERSION_INSTANCE_ID,
+    [SemanticResourceAttributes.SERVICE_NAMESPACE]: serviceNamespace ?? process.env.OTEL_SERVICE_NAMESPACE,
+    [SemanticResourceAttributes.DEPLOYMENT_ENVIRONMENT]: environment ?? process.env.OTEL_DEPLOYMENT_ENVIRONMENT,
+  })
+
+  const provider = new NodeTracerProvider({
+    resource: new Resource(resourceAttributes),
+  });
+
+  provider.addSpanProcessor(processor);
+  provider.register({ propagator });
+
+  registerInstrumentations({
+    tracerProvider: provider,
+    instrumentations: [getNodeAutoInstrumentations()],
+  });
+
+  initialized = true;
+};

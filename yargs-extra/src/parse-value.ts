ef2f15ada02ca7eab8ec2f1952054f726f158280
Francois Levasseur <francois.levasseur@botpress.com>
2023-05-12 16:25:23 -0400

    feat(yargs-extra): allow passing array options as environement variables

--- yargs-extra/src/parse-value.ts
+++ yargs-extra/src/parse-value.ts
@@ -1,13 +1,13 @@
 import yargs from 'yargs'
 import yn from 'yn'
 
-export const parseValue = <O extends yargs.Options>(
+const parseSingleValue = <O extends yargs.Options>(
   yargSchema: O,
   envVarValue: string
 ): yargs.InferredOptionType<O> | undefined => {
   if (yargSchema.type === 'string') {
     const parsed: string = envVarValue
-    return parsed as any // typescript is dumb
+    return parsed as yargs.InferredOptionType<O>
   }
 
   if (yargSchema.type === 'number') {
@@ -15,16 +15,30 @@ export const parseValue = <O extends yargs.Options>(
     if (isNaN(parsed)) {
       return
     }
-    return parsed as any // typescript is dumb
+    return parsed as yargs.InferredOptionType<O>
   }
 
   if (yargSchema.type === 'boolean') {
     const parsed: boolean = !!yn(envVarValue)
-    return parsed as any // typescript is dumb
+    return parsed as yargs.InferredOptionType<O>
   }
 
   if (yargSchema.choices?.includes(envVarValue)) {
     const parsed: string = envVarValue
-    return parsed as any // typescript is dumb
+    return parsed as yargs.InferredOptionType<O>
   }
+}
+
+export const parseValue = <O extends yargs.Options>(
+  yargSchema: O,
+  envVarValue: string
+): yargs.InferredOptionType<O> | undefined => {
+  if (yargSchema.array) {
+    const parts = envVarValue.split(' ')
+    const parsed = parts
+      .map((part) => parseSingleValue(yargSchema, part))
+      .filter(<T>(v: T | undefined): v is T => v !== undefined)
+    return parsed as yargs.InferredOptionType<O>
+  }
+  return parseSingleValue(yargSchema, envVarValue)
 }

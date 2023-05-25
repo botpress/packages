eaa92bc7e3238c1bb3c50db2a5230c261b9f2538
franklevasseur <francois_levasseur@hotmail.com>
2023-05-25 16:36:08 -0400

    rm unused tests

--- entities/rssrc/lib.rs
+++ entities/rssrc/lib.rs
@@ -7,12 +7,6 @@ mod tokens;
 
 extern crate console_error_panic_hook;
 
-#[wasm_bindgen]
-extern "C" {
-    #[wasm_bindgen(js_namespace = console)]
-    fn log(s: &str);
-}
-
 fn init() {
     console_error_panic_hook::set_once();
 }

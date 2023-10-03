68fc0b87e8c61a95c500a9952423629f32d89c0f
Francois Levasseur <francois.levasseur@botpress.com>
2023-10-03 10:54:42 -0400

    fix: do not add unkown and internal errors if they are already defined

--- opapi/src/util.ts
+++ opapi/src/util.ts
@@ -22,3 +22,15 @@ export function removeFromArray(array: string[], item: string) {
     array.splice(index, 1)
   }
 }
+
+export function uniqueBy<T, K extends keyof T>(array: readonly T[], k: K): T[] {
+  const seen = new Set<T[K]>()
+  return array.filter((item) => {
+    const v = item[k]
+    if (seen.has(v)) {
+      return false
+    }
+    seen.add(v)
+    return true
+  })
+}

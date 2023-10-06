0ea379b56d963d0725b770cc6b323bef6df9317f
Francois Levasseur <francois.levasseur@botpress.com>
2023-10-06 13:59:44 -0400

    feat(const): integration per workspace count is a quota, not limit

--- const/src/limits.ts
+++ const/src/limits.ts
@@ -15,66 +15,62 @@ export type LimitUnit = 'count' | 'bytes'
 export const limitConfigs = {
   bot_configuration_bytes: {
     value: 20480, // 20 KB
-    unit: 'bytes',
+    unit: 'bytes'
   },
   bot_installed_integrations_count: {
     value: 30,
-    unit: 'count',
+    unit: 'count'
   },
   bot_integration_configuration_bytes: {
     value: 20480, // 20 KB
-    unit: 'bytes',
+    unit: 'bytes'
   },
   bot_recurring_event_count: {
     value: 5,
-    unit: 'count',
+    unit: 'count'
   },
   bot_recurring_event_payload_bytes: {
     value: 131072, // 128 KB
-    unit: 'bytes',
+    unit: 'bytes'
   },
   event_definition_count: {
     value: 20,
-    unit: 'count',
+    unit: 'count'
   },
   integration_channel_count: {
     value: 10,
-    unit: 'count',
+    unit: 'count'
   },
   integration_channel_message_type_count: {
     value: 20,
-    unit: 'count',
+    unit: 'count'
   },
   integration_identifier_count: {
     value: 50,
-    unit: 'count',
+    unit: 'count'
   },
   integration_version_count: {
     value: 50,
-    unit: 'count',
-  },
-  integrations_owned_count: {
-    value: 20,
-    unit: 'count',
+    unit: 'count'
   },
   message_payload_bytes: {
     value: 131072, // 128 KB
-    unit: 'bytes',
+    unit: 'bytes'
   },
   schema_bytes: {
     value: 102400, // 100 KB
-    unit: 'bytes',
+    unit: 'bytes'
   },
   state_definition_count: {
     value: 5,
-    unit: 'count',
+    unit: 'count'
   },
   state_item_payload_bytes: {
     value: 131072, // 128 KB
-    unit: 'bytes',
+    unit: 'bytes'
   },
   tag_definition_count: {
     value: 50,
-    unit: 'count',
-  },
+    unit: 'count'
+  }
 } as const satisfies Record<string, Limit>

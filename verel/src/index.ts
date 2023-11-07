5804a440ae584662f64abaa6ca355ac26f7a8a03
François Levasseur <francois.levasseur@botpress.com>
2023-11-07 15:10:02 -0500

    fix(verel): init only when calling verel (#75)

--- verel/src/index.ts
+++ verel/src/index.ts
@@ -1,4 +1,16 @@
+import { isBrowser } from 'browser-or-node'
 import * as wsm from '../pkg'
+
+let initialized = false
+const maybeInitialize = () => {
+  if (initialized) {
+    return
+  }
+  if (isBrowser) {
+    wsm.init() // this might break on some browsers
+  }
+  initialized = true
+}
 
 export type CompilationDiagnosticLabel = {
   message: string
@@ -19,9 +31,18 @@ export type CompilationDiagnostic = {
   notes: CompilationDiagnosticNote[]
 }
 export type CheckResult = { warnings: CompilationDiagnostic[]; errors: CompilationDiagnostic[] }
-export const check = (program: string): CheckResult => wsm.check(program)
+export const check = (program: string): CheckResult => {
+  maybeInitialize()
+  return wsm.check(program)
+}
 
 export type ExecutionResult = { event: any; result: any }
-export const execute = (program: string, event: any): ExecutionResult => wsm.execute(program, event)
+export const execute = (program: string, event: any): ExecutionResult => {
+  maybeInitialize()
+  return wsm.execute(program, event)
+}
 
-export const formatDiagnostic = (diagnostic: CompilationDiagnostic): string => wsm.format_diagnostic(diagnostic)
+export const formatDiagnostic = (diagnostic: CompilationDiagnostic): string => {
+  maybeInitialize()
+  return wsm.format_diagnostic(diagnostic)
+}

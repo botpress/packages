b79d71428c8e2ac1c5d22ed87c9df033155b2518
Charles Catta <charles.catta@botpress.com>
2024-01-31 16:56:18 -0500

    Refactor

--- zui/src/index.ts
+++ zui/src/index.ts
@@ -1,9 +1,18 @@
-export { getZuiSchemas } from './zui-schemas'
-export { zui, type Zui } from './zui'
-export type { ZuiType, Infer, ZuiExtension, ZuiRawShape, ZuiTypeAny } from './zui'
+export {
+  type UIExtension,
+  type BaseType,
+  type GlobalExtensionDefinition,
+  type UIExtensionDefinition,
+  defaultExtensions,
+} from './uiextensions'
+export { type ZUIReactComponent, type ZUIReactComponentLibrary, defaultComponentLibrary } from './react'
+export type { Zui, ZuiType, Infer, ZuiExtension, ZuiRawShape, ZuiTypeAny } from './zui'
 export type {
   JsonSchema7Type as JsonSchema7,
   JsonSchema7ObjectType as JsonSchema7Object,
 } from '@bpinternal/zod-to-json-schema'
+
+export { zui } from './zui'
+export { getZuiSchemas } from './zui-schemas'
 export { jsonSchemaToZui } from './json-schema-to-zui'
 export { ZodError as ZuiError } from 'zod'

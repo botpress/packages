7172f32062cb0782b8b453761a3d087f220254fc
Francois Levasseur <francois.levasseur@botpress.com>
2024-02-07 10:10:20 -0500

    feat(es-node): like ts-node but using esbuild

---
+++ es-node/src/utils/require-utils.ts
@@ -0,0 +1,15 @@
+import Module from 'module'
+import pathlib from 'path'
+
+export const requireJsCode = <T>(code: string): T => {
+  const filedir = 'tmp'
+  const filename = `${Date.now()}.js`
+
+  const fileid = pathlib.join(filedir, filename)
+
+  const m = new Module(fileid)
+  m.filename = filename
+  // @ts-ignore
+  m._compile(code, filename)
+  return m.exports
+}

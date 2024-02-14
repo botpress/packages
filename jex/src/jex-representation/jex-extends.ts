f8658eeea04cc42f2f13ee83b9d876737b55945b
Francois Levasseur <francois.levasseur@botpress.com>
2024-02-14 11:25:24 -0500

    chore(jex): change child/parent to type A and B

--- jex/src/jex-representation/jex-extends.ts
+++ jex/src/jex-representation/jex-extends.ts
@@ -3,79 +3,77 @@ import { jexEquals } from './jex-equals'
 
 type LiteralOf<T extends types.JexPrimitive> = Extract<types.JexLiteral, { type: T['type'] }>
 
-const _primitiveExtends = <T extends types.JexPrimitive>(child: T | LiteralOf<T>, parent: types.JexType): boolean => {
-  const isT = (x: types.JexType): x is T | LiteralOf<T> => x.type === child.type
-  if (!isT(parent)) return false
+const _primitiveExtends = <T extends types.JexPrimitive>(typeA: T | LiteralOf<T>, typeB: types.JexType): boolean => {
+  const isT = (x: types.JexType): x is T | LiteralOf<T> => x.type === typeA.type
+  if (!isT(typeB)) return false
 
   type Primitive = LiteralOf<T> | (T & { value: undefined })
   const asPrimitive = (value: T | LiteralOf<T>): Primitive =>
     'value' in value ? value : { ...value, value: undefined }
 
-  const _child = asPrimitive(child)
-  const _parent = asPrimitive(parent)
+  const _typeA = asPrimitive(typeA)
+  const _typeB = asPrimitive(typeB)
 
-  if (_parent.value !== undefined && _child.value === undefined) {
+  if (_typeB.value !== undefined && _typeA.value === undefined) {
     return false
   }
-  if (_parent.value !== undefined && _child.value !== _parent.value) {
+  if (_typeB.value !== undefined && _typeA.value !== _typeB.value) {
     return false
   }
   return true
 }
 
-export const jexExtends = (child: types.JexType, parent: types.JexType): boolean => {
-  if (parent.type === 'any' || child.type === 'any') {
+export const jexExtends = (typeA: types.JexType, typeB: types.JexType): boolean => {
+  if (typeB.type === 'any' || typeA.type === 'any') {
     return true
   }
 
-  if (child.type === 'union') {
-    return child.anyOf.every((c) => jexExtends(c, parent))
+  if (typeA.type === 'union') {
+    return typeA.anyOf.every((c) => jexExtends(c, typeB))
   }
 
-  if (parent.type === 'union') {
-    return parent.anyOf.some((p) => jexExtends(child, p))
+  if (typeB.type === 'union') {
+    return typeB.anyOf.some((p) => jexExtends(typeA, p))
   }
 
-  if (child.type === 'object') {
-    if (parent.type === 'map') {
-      return Object.values(child.properties).every((c) => jexExtends(c, parent.items))
+  if (typeA.type === 'object') {
+    if (typeB.type === 'map') {
+      return Object.values(typeA.properties).every((c) => jexExtends(c, typeB.items))
     }
-    if (parent.type === 'object') {
-      return Object.entries(parent.properties).every(([key, parentValue]) => {
-        const childValue = child.properties[key]
-        if (childValue === undefined) return false
-        return jexExtends(childValue, parentValue)
+    if (typeB.type === 'object') {
+      return Object.entries(typeB.properties).every(([key, valueB]) => {
+        const valueA = typeA.properties[key]
+        if (valueA === undefined) return false
+        return jexExtends(valueA, valueB)
       })
     }
     return false
   }
 
-  if (child.type === 'map') {
-    if (parent.type !== 'map') return false
-    return jexExtends(child.items, parent.items)
+  if (typeA.type === 'map') {
+    if (typeB.type !== 'map') return false
+    return jexExtends(typeA.items, typeB.items)
   }
 
-  if (child.type === 'tuple') {
-    if (parent.type === 'array') {
-      return child.items.every((c) => jexExtends(c, parent.items))
+  if (typeA.type === 'tuple') {
+    if (typeB.type === 'array') {
+      return typeA.items.every((c) => jexExtends(c, typeB.items))
     }
-    if (parent.type === 'tuple') {
-      const zipped = child.items.map(
-        (c, i) => [c, parent.items[i]] satisfies [types.JexType, types.JexType | undefined]
-      )
+    if (typeB.type === 'tuple') {
+      const zipped = typeA.items.map((c, i) => [c, typeB.items[i]] satisfies [types.JexType, types.JexType | undefined])
       return zipped.every(([c, p]) => p === undefined || jexExtends(c, p))
     }
     return false
   }
 
-  if (child.type === 'array') {
-    if (parent.type !== 'array') return false
-    return jexExtends(child.items, parent.items)
+  if (typeA.type === 'array') {
+    if (typeB.type !== 'array') return false
+    return jexExtends(typeA.items, typeB.items)
   }
 
-  if (child.type === 'string' || child.type === 'number' || child.type === 'boolean') {
-    return _primitiveExtends(child, parent)
+  if (typeA.type === 'string' || typeA.type === 'number' || typeA.type === 'boolean') {
+    return _primitiveExtends(typeA, typeB)
   }
 
-  return jexEquals(child, parent)
+  return jexEquals(typeA, typeB)
 }

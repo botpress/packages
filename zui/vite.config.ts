d59a8baa52e72cc220be23eeca3ae992c256100d
Charles Catta <charles.catta@botpress.com>
2024-04-04 12:33:52 -0400

    Add zod in repo

--- zui/vite.config.ts
+++ zui/vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
+import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      zod: path.resolve(__dirname, './src/zod/index.ts'),
+    },
+  },
   test: {
-    // 👋 add the line below to add jsdom to vite
     environment: 'jsdom',
     globals: true,
   },

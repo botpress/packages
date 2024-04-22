8bb0df5455046ef2ac6cd7124f71c9f456c32bb2
Charles Catta <charles.catta@botpress.com>
2024-04-22 18:08:01 -0400

    Fix polyfill issue

--- zui/vite.config.ts
+++ zui/vite.config.ts
@@ -5,12 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    nodePolyfills({
-      include: ['path', 'process'],
-    }),
-  ],
+  plugins: [react()],
   test: {
     environment: 'jsdom',
     globals: true,

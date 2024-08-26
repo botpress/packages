28eaa77d661a1929d21e4cf59ad1743683511116
Francois Levasseur <francois.levasseur@botpress.com>
2024-08-26 14:16:41 -0400

    chore(zui): move studio component definitions in zui as defaults

--- zui/src/ui/index.ts
+++ zui/src/ui/index.ts
@@ -1,2 +1,18 @@
 export { ZuiForm, type ZuiFormProps } from './Form'
-export { getSchemaType } from './utils'
+
+export type { DefaultComponentDefinitions } from './component-definitions'
+export type { BoundaryFallbackComponent } from './ErrorBoundary'
+
+export type {
+  BaseType,
+  UIComponentDefinitions,
+  ZuiComponentMap,
+  AsBaseType,
+  ZuiReactComponent,
+  ZuiReactComponentProps,
+  JSONSchema,
+  JSONSchemaOfType,
+  MergeUIComponentDefinitions,
+  FormValidation,
+  FormError,
+} from './types'

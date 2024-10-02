4044e736eade1eedd206a40caddbab5426741520
Francois Levasseur <francois.levasseur@botpress.com>
2024-10-02 19:26:52 -0400

    update

--- zui/src/transforms/zui-to-typescript-schema/index.test.ts
+++ zui/src/transforms/zui-to-typescript-schema/index.test.ts
@@ -1,258 +1,194 @@
 import { describe, expect } from 'vitest'
 import { toTypescriptSchema as toTypescript } from '.'
-import z from '../../z'
 import { evalZuiString } from '../common/eval-zui-string'
+
+const assert = (_expected: string) => ({
+  toGenerateItself: async () => {
+    const schema = evalZuiString(_expected)
+    const actual = toTypescript(schema)
+    await expect(actual).toMatchWithoutFormatting(_expected)
+  },
+  toThrowErrorWhenGenerating: async () => {
+    const schema = evalZuiString(_expected)
+    const fn = () => toTypescript(schema)
+    expect(fn).toThrowError()
+  },
+})
 
 describe('toTypescriptZuiString', () => {
   test('string', async () => {
-    const expected = `z.string()`
-    const schema = evalZuiString(expected)
-    const actual = toTypescript(schema)
-    await expect(actual).toMatchWithoutFormatting(expected)
+    const schema = `z.string()`
+    await assert(schema).toGenerateItself()
   })
   test('number', async () => {
-    const expected = `z.number()`
-    const schema = evalZuiString(expected)
-    const actual = toTypescript(schema)
-    await expect(actual).toMatchWithoutFormatting(expected)
+    const schema = `z.number()`
+    await assert(schema).toGenerateItself()
   })
   test('nan', async () => {
-    const expected = `z.nan()`
-    const schema = evalZuiString(expected)
-    const actual = toTypescript(schema)
-    await expect(actual).toMatchWithoutFormatting(expected)
+    const schema = `z.nan()`
+    await assert(schema).toGenerateItself()
   })
   test('bigint', async () => {
-    const expected = `z.bigint()`
-    const schema = evalZuiString(expected)
-    const actual = toTypescript(schema)
-    await expect(actual).toMatchWithoutFormatting(expected)
+    const schema = `z.bigint()`
+    await assert(schema).toGenerateItself()
   })
   test('boolean', async () => {
-    const expected = `z.boolean()`
-    const schema = evalZuiString(expected)
-    const actual = toTypescript(schema)
-    await expect(actual).toMatchWithoutFormatting(expected)
+    const schema = `z.boolean()`
+    await assert(schema).toGenerateItself()
   })
   test('date', async () => {
-    const expected = `z.date()`
-    const schema = evalZuiString(expected)
-    const actual = toTypescript(schema)
-    await expect(actual).toMatchWithoutFormatting(expected)
+    const schema = `z.date()`
+    await assert(schema).toGenerateItself()
   })
   test('undefined', async () => {
-    const expected = `z.undefined()`
-    const schema = evalZuiString(expected)
-    const actual = toTypescript(schema)
-    await expect(actual).toMatchWithoutFormatting(expected)
+    const schema = `z.undefined()`
+    await assert(schema).toGenerateItself()
   })
   test('null', async () => {
-    const expected = `z.null()`
-    const schema = evalZuiString(expected)
-    const actual = toTypescript(schema)
-    await expect(actual).toMatchWithoutFormatting(expected)
+    const schema = `z.null()`
+    await assert(schema).toGenerateItself()
   })
   test('any', async () => {
-    const expected = `z.any()`
-    const schema = evalZuiString(expected)
-    const actual = toTypescript(schema)
-    await expect(actual).toMatchWithoutFormatting(expected)
+    const schema = `z.any()`
+    await assert(schema).toGenerateItself()
   })
   test('unknown', async () => {
-    const expected = `z.unknown()`
-    const schema = evalZuiString(expected)
-    const actual = toTypescript(schema)
-    await expect(actual).toMatchWithoutFormatting(expected)
+    const schema = `z.unknown()`
+    await assert(schema).toGenerateItself()
   })
   test('never', async () => {
-    const expected = `z.never()`
-    const schema = evalZuiString(expected)
-    const actual = toTypescript(schema)
-    await expect(actual).toMatchWithoutFormatting(expected)
+    const schema = `z.never()`
+    await assert(schema).toGenerateItself()
   })
   test('void', async () => {
-    const expected = `z.void()`
-    const schema = evalZuiString(expected)
-    const actual = toTypescript(schema)
-    await expect(actual).toMatchWithoutFormatting(expected)
+    const schema = `z.void()`
+    await assert(schema).toGenerateItself()
   })
   test('array', async () => {
-    const expected = `z.array(z.string())`
-    const schema = evalZuiString(expected)
-    const actual = toTypescript(schema)
-    await expect(actual).toMatchWithoutFormatting(expected)
+    const schema = `z.array(z.string())`
+    await assert(schema).toGenerateItself()
   })
   test('object', async () => {
-    const expected = `z.object({
+    const schema = `z.object({
       a: z.string(),
       b: z.number(),
     })`
-    const schema = evalZuiString(expected)
-    const actual = toTypescript(schema)
-    await expect(actual).toMatchWithoutFormatting(expected)
+    await assert(schema).toGenerateItself()
   })
   test('union', async () => {
-    const expected = `z.union([z.string(), z.number(), z.boolean()])`
-    const schema = evalZuiString(expected)
-    const actual = toTypescript(schema)
-    await expect(actual).toMatchWithoutFormatting(expected)
+    const schema = `z.union([z.string(), z.number(), z.boolean()])`
+    await assert(schema).toGenerateItself()
   })
   test('discriminatedUnion', async () => {
-    const expected = `z.discriminatedUnion("type", [
+    const schema = `z.discriminatedUnion("type", [
       z.object({ type: z.literal("A"), a: z.string() }),
       z.object({ type: z.literal("B"), b: z.number() }),
     ])`
-    const schema = evalZuiString(expected)
-    const actual = toTypescript(schema)
-    await expect(actual).toMatchWithoutFormatting(expected)
+    await assert(schema).toGenerateItself()
   })
   test('intersection', async () => {
-    const expected = `z.intersection(z.object({ a: z.string() }), z.object({ b: z.number() }))`
-    const schema = evalZuiString(expected)
-    const actual = toTypescript(schema)
-    await expect(actual).toMatchWithoutFormatting(expected)
+    const schema = `z.intersection(z.object({ a: z.string() }), z.object({ b: z.number() }))`
+    await assert(schema).toGenerateItself()
   })
   test('tuple', async () => {
-    const expected = `z.tuple([z.string(), z.number()])`
-    const schema = evalZuiString(expected)
-    const actual = toTypescript(schema)
-    await expect(actual).toMatchWithoutFormatting(expected)
+    const schema = `z.tuple([z.string(), z.number()])`
+    await assert(schema).toGenerateItself()
   })
   test('record', async () => {
-    const expected = `z.record(z.string(), z.number())`
-    const schema = evalZuiString(expected)
-    const actual = toTypescript(schema)
-    await expect(actual).toMatchWithoutFormatting(expected)
+    const schema = `z.record(z.string(), z.number())`
+    await assert(schema).toGenerateItself()
   })
   test('map', async () => {
-    const expected = `z.map(z.string(), z.number())`
-    const schema = evalZuiString(expected)
-    const actual = toTypescript(schema)
-    await expect(actual).toMatchWithoutFormatting(expected)
+    const schema = `z.map(z.string(), z.number())`
+    await assert(schema).toGenerateItself()
   })
   test('set', async () => {
-    const expected = `z.set(z.string())`
-    const schema = evalZuiString(expected)
-    const actual = toTypescript(schema)
-    await expect(actual).toMatchWithoutFormatting(expected)
+    const schema = `z.set(z.string())`
+    await assert(schema).toGenerateItself()
   })
   test('function with no argument', async () => {
-    const expected = `z.function().returns(z.void())`
-    const schema = evalZuiString(expected)
-    const actual = toTypescript(schema)
-    await expect(actual).toMatchWithoutFormatting(expected)
+    const schema = `z.function().returns(z.void())`
+    await assert(schema).toGenerateItself()
   })
   test('function with multiple arguments', async () => {
-    const expected = `z.function().args(z.number(), z.string()).returns(z.boolean())`
-    const schema = evalZuiString(expected)
-    const actual = toTypescript(schema)
-    await expect(actual).toMatchWithoutFormatting(expected)
+    const schema = `z.function().args(z.number(), z.string()).returns(z.boolean())`
+    await assert(schema).toGenerateItself()
   })
   test('lazy', async () => {
-    const expected = `z.lazy(() => z.string())`
-    const schema = evalZuiString(expected)
-    const actual = toTypescript(schema)
-    await expect(actual).toMatchWithoutFormatting(expected)
+    const schema = `z.lazy(() => z.string())`
+    await assert(schema).toGenerateItself()
   })
   test('literal string', async () => {
-    const expected = `z.literal("banana")`
-    const schema = evalZuiString(expected)
-    const actual = toTypescript(schema)
-    await expect(actual).toMatchWithoutFormatting(expected)
+    const schema = `z.literal("banana")`
+    await assert(schema).toGenerateItself()
   })
   test('literal number', async () => {
-    const expected = `z.literal(42)`
-    const schema = evalZuiString(expected)
-    const actual = toTypescript(schema)
-    await expect(actual).toMatchWithoutFormatting(expected)
+    const schema = `z.literal(42)`
+    await assert(schema).toGenerateItself()
   })
   test('literal boolean', async () => {
-    const expected = `z.literal(true)`
-    const schema = evalZuiString(expected)
-    const actual = toTypescript(schema)
-    await expect(actual).toMatchWithoutFormatting(expected)
+    const schema = `z.literal(true)`
+    await assert(schema).toGenerateItself()
   })
   test('enum', async () => {
-    const expected = `z.enum(["banana", "apple", "orange"])`
-    const schema = evalZuiString(expected)
-    const actual = toTypescript(schema)
-    await expect(actual).toMatchWithoutFormatting(expected)
+    const schema = `z.enum(["banana", "apple", "orange"])`
+    await assert(schema).toGenerateItself()
   })
-  test('effects', () => {
-    const schema = z.string().transform((s) => s.toUpperCase())
-    const fn = () => toTypescript(schema)
-    expect(fn).toThrowError() // not supported
+  test('effects', async () => {
+    const schema = `z.string().transform((s) => s.toUpperCase())`
+    await assert(schema).toThrowErrorWhenGenerating()
   })
   test('nativeEnum', async () => {
-    enum Fruit {
-      Banana = 'banana',
-      Apple = 'apple',
-      Orange = 'orange',
-    }
-    const schema = z.nativeEnum(Fruit)
-    const fn = () => toTypescript(schema)
-    expect(fn).toThrowError() // not supported
+    const schema = `z.nativeEnum({
+        Banana: 'banana',
+        Apple: 'apple',
+        Orange: 'orange',
+      })
+    `
+    await assert(schema).toThrowErrorWhenGenerating()
   })
   test('optional', async () => {
-    const expected = `z.optional(z.string())`
-    const schema = evalZuiString(expected)
-    const actual = toTypescript(schema)
-    await expect(actual).toMatchWithoutFormatting(expected)
+    const schema = `z.optional(z.string())`
+    await assert(schema).toGenerateItself()
   })
   test('nullable', async () => {
-    const expected = `z.nullable(z.string())`
-    const schema = evalZuiString(expected)
-    const actual = toTypescript(schema)
-    await expect(actual).toMatchWithoutFormatting(expected)
+    const schema = `z.nullable(z.string())`
+    await assert(schema).toGenerateItself()
   })
   test('default', async () => {
-    const expected = `z.string().default('banana')` // TODO: should use `z.default(z.string(), 'banana')` for uniformity
-    const schema = evalZuiString(expected)
-    const actual = toTypescript(schema)
-    await expect(actual).toMatchWithoutFormatting(expected)
+    const schema = `z.string().default('banana')` // TODO: should use `z.default(z.string(), 'banana')` for uniformity
+    await assert(schema).toGenerateItself()
   })
-  test('catch', () => {
-    const expected = `z.string().catch('banana')` // TODO: should use `z.catch(z.string(), 'banana')` for uniformity
-    const schema = evalZuiString(expected)
-    const actual = toTypescript(schema)
-    expect(actual).toMatch(expected)
+  test('catch', async () => {
+    const schema = `z.string().catch('banana')` // TODO: should use `z.catch(z.string(), 'banana')` for uniformity
+    await assert(schema).toGenerateItself()
   })
   test('promise', async () => {
-    const expected = `z.promise(z.string())`
-    const schema = evalZuiString(expected)
-    const actual = toTypescript(schema)
-    await expect(actual).toMatchWithoutFormatting(expected)
+    const schema = `z.promise(z.string())`
+    await assert(schema).toGenerateItself()
   })
-  test('branded', () => {
-    const schema = z.string().brand('MyString')
-    const fn = () => toTypescript(schema)
-    expect(fn).toThrowError() // not supported
+  test('branded', async () => {
+    const schema = `z.string().brand('MyString')`
+    await assert(schema).toThrowErrorWhenGenerating()
   })
   test('pipeline', async () => {
-    const schema = z.pipeline(z.string(), z.number())
-    const fn = () => toTypescript(schema)
-    expect(fn).toThrowError() // not supported
+    const schema = `z.pipeline(z.string(), z.number())`
+    await assert(schema).toThrowErrorWhenGenerating()
   })
   test('symbol', async () => {
-    const schema = z.symbol()
-    const fn = () => toTypescript(schema)
-    expect(fn).toThrowError() // not supported
+    const schema = `z.symbol()`
+    await assert(schema).toThrowErrorWhenGenerating()
   })
   test('readonly', async () => {
-    const expected = `z.readonly(z.string())`
-    const schema = evalZuiString(expected)
-    const actual = toTypescript(schema)
-    await expect(actual).toMatchWithoutFormatting(expected)
+    const schema = `z.readonly(z.string())`
+    await assert(schema).toGenerateItself()
   })
   test('ref', async () => {
-    const expected = `z.ref("#item")`
-    const schema = evalZuiString(expected)
-    const actual = toTypescript(schema)
-    await expect(actual).toMatchWithoutFormatting(expected)
+    const schema = `z.ref("#item")`
+    await assert(schema).toGenerateItself()
   })
   test('templateLiteral', async () => {
-    const schema = z.templateLiteral()
-    const fn = () => toTypescript(schema)
-    expect(fn).toThrowError() // not supported
+    const schema = `z.templateLiteral()`
+    await assert(schema).toThrowErrorWhenGenerating()
   })
 })

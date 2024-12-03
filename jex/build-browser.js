6538c0677b025d84f2ab8c4fbaadccab27d96744
Sylvain Perron <sylvain.perron@botpress.com>
2024-12-03 15:49:53 -0500

    fix(jex): browser build

---
+++ jex/build-browser.js
@@ -0,0 +1,13 @@
+const esbuild = require('esbuild')
+const { polyfillNode } = require('esbuild-plugin-polyfill-node')
+
+esbuild
+  .build({
+    entryPoints: ['src/index.ts'],
+    bundle: true,
+    platform: 'browser',
+    format: 'esm',
+    outfile: 'dist/index.js',
+    plugins: [polyfillNode()]
+  })
+  .catch(() => process.exit(1))

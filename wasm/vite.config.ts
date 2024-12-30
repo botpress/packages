9641059c4cd28bd10a51fe886296b8c9884aeccf
Sylvain Perron <1315508+slvnperron@users.noreply.github.com>
2024-12-30 18:05:36 -0500

    feat(zai): added zai, vai and wasm (#524)

---
+++ wasm/vite.config.ts
@@ -0,0 +1,28 @@
+import wasm from 'vite-plugin-wasm'
+import { defineConfig } from 'vite'
+import topLevelAwait from 'vite-plugin-top-level-await'
+import dts from 'vite-plugin-dts'
+
+export default defineConfig({
+  plugins: [topLevelAwait(), wasm(), dts()],
+  resolve: {
+    alias: {
+      path: 'path-browserify'
+    }
+  },
+  build: {
+    lib: {
+      entry: 'src/tokenizer.ts',
+      formats: ['es'],
+      fileName: (format) => `tokenizer.js`
+    },
+    rollupOptions: {
+      input: 'src/tokenizer.ts',
+      output: {
+        // ensure that the wasm file is included in the same chunk as the js file
+        manualChunks: () => 'tokenizer'
+      },
+      external: (id) => /\.test\.ts$/.test(id) || id.includes('__tests')
+    }
+  }
+})

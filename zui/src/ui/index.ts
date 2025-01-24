edb2109999cf2bd396fdc770c936156369c0a0dc
allardy <yann.allard1@gmail.com>
2025-01-24 12:18:47 -0500

    guards

--- zui/src/ui/index.ts
+++ zui/src/ui/index.ts
@@ -3,6 +3,8 @@ export { ZuiForm, type ZuiFormProps } from './Form'
 export * from './component-definitions'
 export * from './ErrorBoundary'
 
+export { isObjectSchema, isArraySchema } from './utils'
+
 export type {
   BaseType,
   UIComponentDefinitions,

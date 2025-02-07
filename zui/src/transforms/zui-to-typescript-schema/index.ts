f38511e7e5ab46c56f8fc65710fab0f3ce495dfa
Pascal Breton <pascal.breton@botpress.com>
2025-02-07 16:03:07 -0500

    feat(zui): preserve descriptions in toTypescriptSchema

--- zui/src/transforms/zui-to-typescript-schema/index.ts
+++ zui/src/transforms/zui-to-typescript-schema/index.ts
@@ -22,40 +22,40 @@ function sUnwrapZod(schema: z.Schema): string {
 
   switch (def.typeName) {
     case z.ZodFirstPartyTypeKind.ZodString:
-      return `${getMultilineComment(def.description)}z.string()`.trim()
+      return `${getMultilineComment(def.description)}z.string()${_maybeDescribe(def.description)}`.trim()
 
     case z.ZodFirstPartyTypeKind.ZodNumber:
-      return `${getMultilineComment(def.description)}z.number()`.trim()
+      return `${getMultilineComment(def.description)}z.number()${_maybeDescribe(def.description)}`.trim()
 
     case z.ZodFirstPartyTypeKind.ZodNaN:
-      return `${getMultilineComment(def.description)}z.nan()`.trim()
+      return `${getMultilineComment(def.description)}z.nan()${_maybeDescribe(def.description)}`.trim()
 
     case z.ZodFirstPartyTypeKind.ZodBigInt:
-      return `${getMultilineComment(def.description)}z.bigint()`.trim()
+      return `${getMultilineComment(def.description)}z.bigint()${_maybeDescribe(def.description)}`.trim()
 
     case z.ZodFirstPartyTypeKind.ZodBoolean:
-      return `${getMultilineComment(schema._def.description)}z.boolean()`.trim()
+      return `${getMultilineComment(schema._def.description)}z.boolean()${_maybeDescribe(schema._def.description)}`.trim()
 
     case z.ZodFirstPartyTypeKind.ZodDate:
-      return `${getMultilineComment(def.description)}z.date()`.trim()
+      return `${getMultilineComment(def.description)}z.date()${_maybeDescribe(def.description)}`.trim()
 
     case z.ZodFirstPartyTypeKind.ZodUndefined:
-      return `${getMultilineComment(def.description)}z.undefined()`.trim()
+      return `${getMultilineComment(def.description)}z.undefined()${_maybeDescribe(def.description)}`.trim()
 
     case z.ZodFirstPartyTypeKind.ZodNull:
-      return `${getMultilineComment(def.description)}z.null()`.trim()
+      return `${getMultilineComment(def.description)}z.null()${_maybeDescribe(def.description)}`.trim()
 
     case z.ZodFirstPartyTypeKind.ZodAny:
-      return `${getMultilineComment(def.description)}z.any()`.trim()
+      return `${getMultilineComment(def.description)}z.any()${_maybeDescribe(def.description)}`.trim()
 
     case z.ZodFirstPartyTypeKind.ZodUnknown:
-      return `${getMultilineComment(def.description)}z.unknown()`.trim()
+      return `${getMultilineComment(def.description)}z.unknown()${_maybeDescribe(def.description)}`.trim()
 
     case z.ZodFirstPartyTypeKind.ZodNever:
-      return `${getMultilineComment(def.description)}z.never()`.trim()
+      return `${getMultilineComment(def.description)}z.never()${_maybeDescribe(def.description)}`.trim()
 
     case z.ZodFirstPartyTypeKind.ZodVoid:
-      return `${getMultilineComment(def.description)}z.void()`.trim()
+      return `${getMultilineComment(def.description)}z.void()${_maybeDescribe(def.description)}`.trim()
 
     case z.ZodFirstPartyTypeKind.ZodArray:
       return `z.array(${sUnwrapZod(def.type)})`
@@ -71,58 +71,58 @@ function sUnwrapZod(schema: z.Schema): string {
         //
         `${getMultilineComment(def.description)}z.object({`,
         ...Object.entries(props).map(([key, value]) => `  ${key}: ${value},`),
-        `})`,
+        `})${_maybeDescribe(def.description)}`,
       ]
         .join('\n')
         .trim()
 
     case z.ZodFirstPartyTypeKind.ZodUnion:
       const options = def.options.map(sUnwrapZod)
-      return `${getMultilineComment(def.description)}z.union([${options.join(', ')}])`.trim()
+      return `${getMultilineComment(def.description)}z.union([${options.join(', ')}])${_maybeDescribe(def.description)}`.trim()
 
     case z.ZodFirstPartyTypeKind.ZodDiscriminatedUnion:
       const opts = (def.options as z.ZodSchema[]).map(sUnwrapZod)
       const discriminator = primitiveToTypescriptValue(def.discriminator)
-      return `${getMultilineComment(def.description)}z.discriminatedUnion(${discriminator}, [${opts.join(', ')}])`.trim()
+      return `${getMultilineComment(def.description)}z.discriminatedUnion(${discriminator}, [${opts.join(', ')}])${_maybeDescribe(def.description)}`.trim()
 
     case z.ZodFirstPartyTypeKind.ZodIntersection:
       const left: string = sUnwrapZod(def.left)
       const right: string = sUnwrapZod(def.right)
-      return `${getMultilineComment(def.description)}z.intersection(${left}, ${right})`.trim()
+      return `${getMultilineComment(def.description)}z.intersection(${left}, ${right})${_maybeDescribe(def.description)}`.trim()
 
     case z.ZodFirstPartyTypeKind.ZodTuple:
       const items = def.items.map(sUnwrapZod)
-      return `${getMultilineComment(def.description)}z.tuple([${items.join(', ')}])`.trim()
+      return `${getMultilineComment(def.description)}z.tuple([${items.join(', ')}])${_maybeDescribe(def.description)}`.trim()
 
     case z.ZodFirstPartyTypeKind.ZodRecord:
       const keyType = sUnwrapZod(def.keyType)
       const valueType = sUnwrapZod(def.valueType)
-      return `${getMultilineComment(def.description)}z.record(${keyType}, ${valueType})`.trim()
+      return `${getMultilineComment(def.description)}z.record(${keyType}, ${valueType})${_maybeDescribe(def.description)}`.trim()
 
     case z.ZodFirstPartyTypeKind.ZodMap:
       const mapKeyType = sUnwrapZod(def.keyType)
       const mapValueType = sUnwrapZod(def.valueType)
-      return `${getMultilineComment(def.description)}z.map(${mapKeyType}, ${mapValueType})`.trim()
+      return `${getMultilineComment(def.description)}z.map(${mapKeyType}, ${mapValueType})${_maybeDescribe(def.description)}`.trim()
 
     case z.ZodFirstPartyTypeKind.ZodSet:
-      return `${getMultilineComment(def.description)}z.set(${sUnwrapZod(def.valueType)})`.trim()
+      return `${getMultilineComment(def.description)}z.set(${sUnwrapZod(def.valueType)})${_maybeDescribe(def.description)}`.trim()
 
     case z.ZodFirstPartyTypeKind.ZodFunction:
       const args = def.args.items.map(sUnwrapZod)
       const argsString = args.length ? `.args(${args.join(', ')})` : ''
       const returns = sUnwrapZod(def.returns)
-      return `${getMultilineComment(def.description)}z.function()${argsString}.returns(${returns})`.trim()
+      return `${getMultilineComment(def.description)}z.function()${argsString}.returns(${returns})${_maybeDescribe(def.description)}`.trim()
 
     case z.ZodFirstPartyTypeKind.ZodLazy:
-      return `${getMultilineComment(def.description)}z.lazy(() => ${sUnwrapZod(def.getter())})`.trim()
+      return `${getMultilineComment(def.description)}z.lazy(() => ${sUnwrapZod(def.getter())})${_maybeDescribe(def.description)}`.trim()
 
     case z.ZodFirstPartyTypeKind.ZodLiteral:
       const value = primitiveToTypescriptValue(def.value)
-      return `${getMultilineComment(def.description)}z.literal(${value})`.trim()
+      return `${getMultilineComment(def.description)}z.literal(${value})${_maybeDescribe(def.description)}`.trim()
 
     case z.ZodFirstPartyTypeKind.ZodEnum:
       const values = def.values.map(primitiveToTypescriptValue)
-      return `${getMultilineComment(def.description)}z.enum([${values.join(', ')}])`.trim()
+      return `${getMultilineComment(def.description)}z.enum([${values.join(', ')}])${_maybeDescribe(def.description)}`.trim()
 
     case z.ZodFirstPartyTypeKind.ZodEffects:
       throw new errors.UnsupportedZuiToTypescriptSchemaError(z.ZodFirstPartyTypeKind.ZodEffects)
@@ -131,21 +131,21 @@ function sUnwrapZod(schema: z.Schema): string {
       throw new errors.UnsupportedZuiToTypescriptSchemaError(z.ZodFirstPartyTypeKind.ZodNativeEnum)
 
     case z.ZodFirstPartyTypeKind.ZodOptional:
-      return `${getMultilineComment(def.description)}z.optional(${sUnwrapZod(def.innerType)})`.trim()
+      return `${getMultilineComment(def.description)}z.optional(${sUnwrapZod(def.innerType)})${_maybeDescribe(def.description)}`.trim()
 
     case z.ZodFirstPartyTypeKind.ZodNullable:
-      return `${getMultilineComment(def.description)}z.nullable(${sUnwrapZod(def.innerType)})`.trim()
+      return `${getMultilineComment(def.description)}z.nullable(${sUnwrapZod(def.innerType)})${_maybeDescribe(def.description)}`.trim()
 
     case z.ZodFirstPartyTypeKind.ZodDefault:
       const defaultValue = unknownToTypescriptValue(def.defaultValue())
       // TODO: use z.default() notation
-      return `${getMultilineComment(def.description)}${sUnwrapZod(def.innerType)}.default(${defaultValue})`.trim()
+      return `${getMultilineComment(def.description)}${sUnwrapZod(def.innerType)}.default(${defaultValue})${_maybeDescribe(def.description)}`.trim()
 
     case z.ZodFirstPartyTypeKind.ZodCatch:
       throw new errors.UnsupportedZuiToTypescriptSchemaError(z.ZodFirstPartyTypeKind.ZodCatch)
 
     case z.ZodFirstPartyTypeKind.ZodPromise:
-      return `${getMultilineComment(def.description)}z.promise(${sUnwrapZod(def.type)})`.trim()
+      return `${getMultilineComment(def.description)}z.promise(${sUnwrapZod(def.type)})${_maybeDescribe(def.description)}`.trim()
 
     case z.ZodFirstPartyTypeKind.ZodBranded:
       throw new errors.UnsupportedZuiToTypescriptSchemaError(z.ZodFirstPartyTypeKind.ZodBranded)
@@ -157,13 +157,15 @@ function sUnwrapZod(schema: z.Schema): string {
       throw new errors.UnsupportedZuiToTypescriptSchemaError(z.ZodFirstPartyTypeKind.ZodSymbol)
 
     case z.ZodFirstPartyTypeKind.ZodReadonly:
-      return `${getMultilineComment(def.description)}z.readonly(${sUnwrapZod(def.innerType)})`.trim()
+      return `${getMultilineComment(def.description)}z.readonly(${sUnwrapZod(def.innerType)})${_maybeDescribe(def.description)}`.trim()
 
     case z.ZodFirstPartyTypeKind.ZodRef:
       const uri = primitiveToTypescriptValue(def.uri)
-      return `${getMultilineComment(def.description)}z.ref(${uri})`.trim()
+      return `${getMultilineComment(def.description)}z.ref(${uri})${_maybeDescribe(def.description)}`.trim()
 
     default:
       util.assertNever(def)
   }
 }
+
+const _maybeDescribe = (description?: string) => (description ? `.describe(${JSON.stringify(description)})` : '')

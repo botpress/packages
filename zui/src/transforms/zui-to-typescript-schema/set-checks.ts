2d419b2d2e1a7dfc2fb0cca4459c39a20c17ab45
Francois Levasseur <francois.levasseur@botpress.com>
2025-02-20 11:57:21 -0500

    update

--- zui/src/transforms/zui-to-typescript-schema/set-checks.ts
+++ zui/src/transforms/zui-to-typescript-schema/set-checks.ts
@@ -1,22 +1,4 @@
 import { primitiveToTypescriptValue as toTs } from '../common/utils'
-// import { ZodArrayDef } from '../../z/types/array'
-
-// export const generateArrayChecks = (def: ZodArrayDef): string => {
-//   const checks: string[] = []
-//   if (def.exactLength) {
-//     const { value, message } = def.exactLength
-//     checks.push(`.length(${toTs(value)}, ${toTs(message)})`)
-//   }
-//   if (def.minLength) {
-//     const { value, message } = def.minLength
-//     checks.push(`.min(${toTs(value)}, ${toTs(message)})`)
-//   }
-//   if (def.maxLength) {
-//     const { value, message } = def.maxLength
-//     checks.push(`.max(${toTs(value)}, ${toTs(message)})`)
-//   }
-//   return checks.join('')
-// }
 import { ZodSetDef } from '../../z/types/set'
 export const generateSetChecks = (def: ZodSetDef): string => {
   const checks: string[] = []

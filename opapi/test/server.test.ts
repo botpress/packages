fc4f69f6bb271bd7e78703b4ddc439cdebf9503c
Francois Levasseur <francois.levasseur@botpress.com>
2025-02-21 11:27:06 -0500

    fix(opapi): post request with empty body results in operation input being Record<string, never>

--- opapi/test/server.test.ts
+++ opapi/test/server.test.ts
@@ -1,9 +1,10 @@
-import { describe, expect, it } from 'vitest'
+import { afterEach, describe, expect, it } from 'vitest'
 import { getMockApi } from './api'
 import { join } from 'path'
-import { existsSync } from 'fs'
+import { existsSync, rmdirSync } from 'fs'
 import { getFiles } from '../src/file'
-import { validateTypescriptFile } from './util'
+import { getTypescriptErrors, validateTypescriptFile } from './util'
+import { z } from 'zod'
 
 const serverFiles = [
   'definition.ts',
@@ -15,9 +16,11 @@ const serverFiles = [
   'errors.ts',
 ]
 
+const GEN_DIR = join(__dirname, 'gen/server')
+
 describe('server generator', () => {
   it('should be able to generate a server', async () => {
-    const genServerFolder = join(__dirname, 'gen/server')
+    const genServerFolder = GEN_DIR
 
     const api = getMockApi()
 
@@ -34,5 +37,46 @@ describe('server generator', () => {
 
     const files = getFiles(genServerFolder)
     expect(files.length).toBe(serverFiles.length)
+  })
+
+  // TODO: enable this test
+  it.skip('should correctly handle empty request body', async () => {
+    const genServerFolder = GEN_DIR
+
+    const api = getMockApi()
+
+    api.addOperation({
+      name: 'postBaz',
+      description: 'Post a baz',
+      method: 'post',
+      path: '/baz/{id}',
+      requestBody: { schema: z.object({}), description: 'Baz information' },
+      response: { schema: z.object({ baz: z.object({ id: z.string() }) }), description: 'Baz information' },
+      parameters: {
+        id: {
+          in: 'path',
+          description: 'Baz id',
+          type: 'string',
+        },
+      },
+    })
+
+    await api.exportServer(genServerFolder, true)
+
+    serverFiles.forEach((file) => {
+      const filename = join(genServerFolder, file)
+      expect(existsSync(filename), `${filename} should exist`).toBe(true)
+
+      const errors = getTypescriptErrors(filename, { strict: true })
+      expect(errors, `${filename} should contain no typescript errors`).toEqual([])
+    })
+
+    const files = getFiles(genServerFolder)
+    expect(files.length).toBe(serverFiles.length)
+  })
+
+  afterEach(() => {
+    const genServerFolder = GEN_DIR
+    rmdirSync(genServerFolder, { recursive: true })
   })
 })
--- opapi/test/util.ts
+++ opapi/test/util.ts
@@ -1,13 +1,25 @@
-import { basename } from 'path'
-import { createProgram } from 'typescript'
+import * as tsc from 'typescript'
 
-export function validateTypescriptFile(filename: string) {
-  const program = createProgram([filename], {})
-  const diags = program.getSyntacticDiagnostics()
+const host: tsc.FormatDiagnosticsHost = {
+  getCurrentDirectory: () => process.cwd(),
+  getCanonicalFileName: (path) => path,
+  getNewLine: () => '\n',
+}
 
-  if (diags.length) {
-    diags.forEach((diag) => {
-      throw new Error(`Error while parsing ${basename(diag.file.fileName)}: ${diag.messageText}`)
-    })
+export function validateTypescriptFile(filename: string, options: tsc.CompilerOptions = {}): void {
+  const program = tsc.createProgram([filename], options)
+  const diags = program.getSyntacticDiagnostics() // TODO: run all diagnostics
+
+  const [diag] = diags
+  if (diag) {
+    const message = tsc.formatDiagnostic(diag, host)
+    throw new Error(`Error while parsing ${filename}:\n${message}`)
   }
+}
+
+export function getTypescriptErrors(filename: string, options: tsc.CompilerOptions = {}): string[] {
+  const program = tsc.createProgram([filename], options)
+  const diagnostics = tsc.getPreEmitDiagnostics(program)
+
+  return diagnostics.map((diag) => tsc.formatDiagnostic(diag, host))
 }

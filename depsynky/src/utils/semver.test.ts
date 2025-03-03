e52dc0ab1ab674ce618a4b85f5d892d6ec48c9b0
Francois Levasseur <francois.levasseur@botpress.com>
2025-03-03 15:00:01 -0500

    update

--- depsynky/src/utils/semver.test.ts
+++ depsynky/src/utils/semver.test.ts
@@ -1,11 +1,15 @@
 import * as semver from './semver'
 import { test, expect } from 'vitest'
 
-test('getLowerBound', () => {
+test('getLowerBound with invalid version should return null', () => {
   expect(semver.getLowerBound('banana')).toEqual(null)
-  expect(semver.getLowerBound('1.2.3')).toEqual('1.2.3')
-  expect(semver.getLowerBound('^1.2.3')).toEqual('1.2.3')
-  expect(semver.getLowerBound('~1.2.3')).toEqual('1.2.3')
-  expect(semver.getLowerBound('>=1.2.3 <2.0.0-0')).toEqual('1.2.3')
-  expect(semver.getLowerBound('>=1.2.3 <1.2.4')).toEqual('1.2.3')
+})
+
+test('getLowerBound with valid version should return the same version', () => {
+  const expected = '1.2.3'
+  expect(semver.getLowerBound('1.2.3')).toEqual(expected)
+  expect(semver.getLowerBound('^1.2.3')).toEqual(expected)
+  expect(semver.getLowerBound('~1.2.3')).toEqual(expected)
+  expect(semver.getLowerBound('>=1.2.3 <2.0.0-0')).toEqual(expected)
+  expect(semver.getLowerBound('>=1.2.3 <1.2.4')).toEqual(expected)
 })

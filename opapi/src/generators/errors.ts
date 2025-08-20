9507b026953e2b6f97a0716c08fcacc73f008103
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-08-19 20:44:05 -0400

    spelling: implementation
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- opapi/src/generators/errors.ts
+++ opapi/src/generators/errors.ts
@@ -51,7 +51,7 @@ let cryptoLib: CryptoLib =
     : crypto
 
 if (!cryptoLib.getRandomValues) {
-  // Use a polyfill in older environments that have a crypto implementaton missing getRandomValues()
+  // Use a polyfill in older environments that have a crypto implementation missing getRandomValues()
   cryptoLib = cryptoLibPolyfill
 }
 

d313faf24ac9378ceeef7af4a278900b66b727e9
François Levasseur <francois_levasseur@hotmail.com>
2026-02-06 15:08:45 -0500

    update

--- thicktoken/src/smart-slice.ts
+++ thicktoken/src/smart-slice.ts
@@ -3,13 +3,13 @@ export type Slice = [number, number]
 export class SmartSlice {
   private _sortedSlices: Slice[]
 
-  public constructor(slices: Slice[], total: number) {
+  public constructor(slices: Slice[], max: number) {
     // merge and sort slices here if needed
 
     const trimmed: Slice[] = []
     for (const [start, end] of slices) {
-      const clampedStart = Math.max(0, Math.min(total, start))
-      const clampedEnd = Math.max(0, Math.min(total, end))
+      const clampedStart = Math.max(0, Math.min(max, start))
+      const clampedEnd = Math.max(0, Math.min(max, end))
       if (clampedStart < clampedEnd) {
         trimmed.push([clampedStart, clampedEnd])
       }
